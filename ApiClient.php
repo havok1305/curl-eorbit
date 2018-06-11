22f87479724ea6baf4ab1a6246797cdd953adbbe
havok1305 <havok1305@gmail.com>
2018-06-11 19:21:50 -0300

    Criado cliente para API do Eorbit

---
+++ ApiClient.php
@@ -0,0 +1,73 @@
+<?php
+
+class ApiClient
+{
+    protected $url = "http://webservice.eorbit.com.br";
+    protected $cliente = 'teste';
+
+    public function __construct($cliente = '')
+    {
+        $this->cliente = $cliente;
+    }
+
+    public function getToken($usuario, $senha)
+    {
+        $url = "/auth";
+
+        $headers = [
+            'Content-Type:application/json',
+            'X-Life-Sistemas-Id-Cliente:'.$this->cliente
+        ];
+
+        $post_data = array(
+            "login"=>$usuario,
+            "senha"=>$senha
+        );
+
+        $resposta = $this->doPost($post_data, $headers, $url);
+
+        if($resposta['status']) {
+            return $resposta['token'];
+        } else {
+            return false;
+        }
+    }
+
+    public function insereMulta($dados, $token)
+    {
+        $url = "/lancphl";
+
+        $headers = [
+            'Content-Type:application/json',
+            'X-Life-Sistemas-Id-Cliente:'.$this->cliente,
+            'Authorization:Bearer '.$token
+        ];
+
+        return $this->doPost($dados, $headers, $url);
+    }
+
+    public function doPost($postdata, $headers, $url)
+    {
+        $url = $this->url . $url;
+
+        $postdata = json_encode($postdata);
+
+        $sessao = curl_init();
+
+        curl_setopt($sessao, CURLOPT_URL, $url);
+
+        curl_setopt($sessao, CURLOPT_HTTPHEADER, $headers);
+
+        curl_setopt($sessao, CURLOPT_POST, true);
+
+        curl_setopt($sessao, CURLOPT_POSTFIELDS, $postdata);
+
+        curl_setopt($sessao, CURLOPT_RETURNTRANSFER, 1);
+
+        $output = curl_exec($sessao);
+
+        curl_close($sessao);
+
+        return json_decode($output, true);
+    }
+}
--- index.php
+++ index.php
@@ -1,6 +1,66 @@
 <?php
-require 'GetToken.php';
+require 'ApiClient.php';
 
-$getToken = new GetToken();
-$getToken->token('elysio', 'elysio', 'teste');
-//$getToken->test();
+$client = new ApiClient('teste');
+
+// pega o token
+// não é necessário gerar um token toda vez que precisar realizar uma requisição
+// é necessário apenas quando ainda não possui um token ou se ele tiver expirado (a cada 60 minutos ele expira)
+$token = $client->getToken('elysio', 'elysio');
+
+if($token) {
+    // inserção para alunos de GRADUAÇÃO
+    $dados = [
+        "tipo"=> "GRA",
+        "matricula"=> "0120130527",
+        "data_vencimento" => "2018-".rand(1,12)."-".rand(1,30),
+        "valor" => rand(1.1,70.99),
+        "mensagem_boleto" => "lançamento teste",
+        "id_multa" => rand(1000,99999)
+    ];
+
+    $resposta = $client->insereMulta($dados, $token);
+    if($resposta['status']) {
+        echo "<br>Lançamento inserido com sucesso.";
+    } else {
+        echo "<br>Não foi possível inserir o lançamento Erro: ".$resposta['message'];
+    }
+
+    // inserção para alunos de POS-GRADUAÇÃO
+    $dados = [
+        "tipo"=> "POS",
+        "matricula"=> "81820160008",
+        "data_vencimento" => "2018-".rand(1,12)."-".rand(1,30),
+        "valor" => rand(1.1,70.99),
+        "mensagem_boleto" => "lançamento teste",
+        "id_multa" => rand(1000,99999)
+    ];
+
+    $resposta = $client->insereMulta($dados, $token);
+
+    if($resposta['status']) {
+        echo "<br>Lançamento inserido com sucesso.";
+    } else {
+        echo "<br>Não foi possível inserir o lançamento Erro: ".$resposta['message'];
+    }
+
+    // inserção para usuários usando o CPF
+    $dados = [
+        "tipo"=> "EXT",
+        "cpf"=> "146.716.418-60",
+        "data_vencimento" => "2018-".rand(1,12)."-".rand(1,30),
+        "valor" => rand(1.1,70.99),
+        "mensagem_boleto" => "lançamento teste",
+        "id_multa" => rand(1000,99999)
+    ];
+
+    $resposta = $client->insereMulta($dados, $token);
+    if($resposta['status']) {
+        echo "<br>Lançamento inserido com sucesso.";
+    } else {
+        echo "<br>Não foi possível inserir o lançamento. Erro: ".$resposta['message'];
+    }
+
+} else {
+    echo "<br>Não foi possível pegar o token.";
+}
